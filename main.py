b279316ac05a6841988110d1ea4686e9429896cf
Kiril Kobzar <kirilkobzar@gmail.com>
2023-04-08 10:22:29 +0300

    added feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Cat:
+    isHungry = True
+
     def __init__(self, name):
         self.name = name
 
@@ -15,11 +17,22 @@ class Person:
         self.name = name
         self.age = age
 
+    def feed(self, cat):
+        if cat.isHungry:
+            print(self.name, "is feeding cat named", cat.name)
+            cat.isHungry = False
+        else:
+            print(cat.name, 'is not hungry')
+
+
+
 
 my_cat = Cat("Barsik")
 me = Person('Kyryl', 22)
 
-print(me.isHappy)
+# print(me.isHappy)
+#
+# my_cat.play(me)
+# print(me.isHappy)
 
-my_cat.play(me)
-print(me.isHappy)
+me.feed(my_cat)
